4a816b29b402cca5d820a3b3e750aebec172ed27
burndmg <dmgburn@gmail.com>
2024-04-04 18:28:13 +0200

    add DateTime scalar

--- user-events-service/src/main/java/net/ecommerce/events/Application.java
+++ user-events-service/src/main/java/net/ecommerce/events/Application.java
@@ -2,8 +2,10 @@ package net.ecommerce.events;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.annotation.ComponentScan;
 
 @SpringBootApplication
+@ComponentScan("net.ecommerce.commons")
 public class Application {
 
     public static void main(String[] args) {
